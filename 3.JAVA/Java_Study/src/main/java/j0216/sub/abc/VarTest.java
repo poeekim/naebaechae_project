ba7595317a66161df3afa01c17af46580656f27a
hyeonakim <hyeonakim0129@gmail.com>
2024-02-20 09:56:28 +0900

    --

---
+++ 3.JAVA/Java_Study/src/main/java/j0216/sub/abc/VarTest.java
@@ -0,0 +1,16 @@
+package j0216.sub.abc;
+
+public class VarTest {
+
+	public static void main(String[] args) {
+		System.out.println("변수의 개요 및 선언 작성법");
+		
+//		자료형 변수명
+		
+		int a;
+		int b = 10;
+		
+		
+
+	}
+}
