704b94e1b89ffa29fe4787340ab395b950704d96
hyeonakim <hyeonakim0129@gmail.com>
2024-02-20 09:47:01 +0900

    -

---
+++ 3.JAVA/Java_Study/src/main/java/j0220/IfTest.java
@@ -0,0 +1,24 @@
+package j0220;
+
+public class IfTest {
+
+    public static void main(String[] args) {
+        int su = 31;
+
+        if(su%2 == 0){
+            System.out.println(su+"은 짝수");
+            System.out.println("블럭처리 연습1");
+
+        }else{
+            System.out.println(su+"은 홀수");
+            System.out.println("블럭처리 연습2");
+
+        }
+
+        // 삼항연산자 -> if문 대체 -> 조건식?참일경우:거짓일경우
+
+        System.out.println("삼항연산자로 변경");
+
+
+    }
+}
